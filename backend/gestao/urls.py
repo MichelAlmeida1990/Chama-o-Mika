5fef8d88524d0a5acdee162ee8cb34d5f8a3d4ef
Michas <michelpaulo06@hotmail.com>
2025-12-19 12:37:17 -0300

    Fix: Adicionar import de populate_mock_data_view

--- backend/gestao/urls.py
+++ backend/gestao/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import login_view, logout_view, user_view
+from .views import login_view, logout_view, user_view, populate_mock_data_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
